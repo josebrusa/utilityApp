0d23de72e6fc56fbbc593c3d04306c30976d50a7
josebrusa <josebrusa@outlook.com>
2024-06-27 12:32:30 +0200

    component Switch create

--- scr/presentation/screens/switches/SwitchScreen.tsx
+++ scr/presentation/screens/switches/SwitchScreen.tsx
@@ -1,23 +1,54 @@
-import { Switch, Text } from 'react-native';
+import { Switch } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
-import { Button } from '../../components/ui/Button';
 import { useState } from 'react';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
+import { Separator } from '../../components/ui/Separator';
 
 export const SwitchScreen = () => {
 
-    const [ isEnabled, setIsEnabled ] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    // const [ isEnabled, setIsEnabled ] = useState(false);
+    // const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    const [ state, setState ] = useState({
+        isActive: true,
+        isHungry: false,
+        isHappy: true,
+    });
 
     return (
         <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
-            <Switch
-                trackColor={{ false: '#767577', true: '#81b0ff' }}
-                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
+            <Card>
+
+                <CustomSwitch
+                    isOn={state.isActive}
+                    onChange={(value) => setState({ ...state, isActive: value })}
+                    text='Activo'
+                />
+                <Separator />
+
+                <CustomSwitch
+                    isOn={state.isHungry}
+                    onChange={(value) => setState({ ...state, isHungry: value })}
+                    text='Tiene Hambre?'
+                />
+                <Separator />
+
+                <CustomSwitch
+                    isOn={state.isHappy}
+                    onChange={(value) => setState({ ...state, isHappy: value })}
+                    text='Esta Feliz?'
+                />
+
+
+                {/* <Switch
+                    trackColor={{ false: '#767577', true: '#8316a4' }}
+                    thumbColor={isEnabled ? '#8316d1' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                /> */}
+            </Card>
 
         </CustomView>
     );
