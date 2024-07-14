0fa0d3c23c9e238569f7831b915ec3e9f20f1a49
josebrusa <josebrusa@outlook.com>
2024-07-14 23:44:31 +0200

    component Section List

--- scr/presentation/navigator/Navigtor.tsx
+++ scr/presentation/navigator/Navigtor.tsx
@@ -6,18 +6,23 @@ import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
+import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 
 const Stack = createStackNavigator()
 export const Navigator = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+
             <Stack.Screen name='HomeScreen' component={HomeScreen} />
             <Stack.Screen name='Animation101Screen' component={Animation101Screen} />
             <Stack.Screen name='Animation102Screen' component={Animation102Screen} />
+
             <Stack.Screen name='SwitchScreen' component={SwitchScreen} />
             <Stack.Screen name='AlertScreen' component={AlertScreen} />
             <Stack.Screen name='TextInputScreen' component={TextInputScreen} />
+
             <Stack.Screen name='PullToRefreshScreen' component={PullToRefreshScreen} />
+            <Stack.Screen name='CustomSectionListScreen' component={CustomSectionListScreen} />
         </Stack.Navigator>
     );
 }
