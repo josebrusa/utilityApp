df30a8191e35c0db0398a1397d5943164dae9f60
josebrusa <josebrusa@outlook.com>
2024-07-15 01:13:27 +0200

    component Infinite Scroll con imagenes

--- scr/presentation/navigator/Navigtor.tsx
+++ scr/presentation/navigator/Navigtor.tsx
@@ -8,6 +8,7 @@ import { TextInputScreen } from '../screens/inputs/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
 import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 import { ModalScreen } from '../screens/ui/ModalScreen';
+import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
 
 const Stack = createStackNavigator()
 export const Navigator = () => {
@@ -25,6 +26,7 @@ export const Navigator = () => {
             <Stack.Screen name='PullToRefreshScreen' component={PullToRefreshScreen} />
             <Stack.Screen name='CustomSectionListScreen' component={CustomSectionListScreen} />
             <Stack.Screen name='ModalScreen' component={ModalScreen} />
+            <Stack.Screen name='InfiniteScrollScreen' component={InfiniteScrollScreen} />
         </Stack.Navigator>
     );
 }
---
+++ scr/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { ActivityIndicator, Image, Text, View } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { FlatList } from 'react-native-gesture-handler';
+import { colors } from '../../../config/theme/theme';
+import { FadeInImage } from '../../components/ui/FadeInImage';
+
+export const InfiniteScrollScreen = () => {
+
+
+    const [ number, setNumber ] = useState([ 0, 1, 2, 3, 4, 5 ])
+
+    const loadMore = () => {
+        //Todo: añadir numero a nuestro arreglo
+        const newArray = Array.from({ length: 5 }, (_, i) => number.length + i)
+
+        setTimeout(() => {
+            // setNumber([ ...number, ...newArray ])
+            setNumber([ ...number, ...newArray ])
+        }, 3000)
+    }
+
+    return (
+        <View style={{ backgroundColor: 'black' }}>
+            <FlatList
+                data={number}
+                onEndReached={loadMore}
+                onEndReachedThreshold={0.6}
+                keyExtractor={item => item.toString()}
+                renderItem={({ item }) => <ListItem number={item} />}
+
+                ListFooterComponent={() => (
+                    <View style={{ height: 150, justifyContent: 'center' }}>
+                        <ActivityIndicator size={40} color={colors.primary} />
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
+
+interface ListItemProps {
+    number: number
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+    return (
+
+
+        <FadeInImage
+            uri={`https://picsum.photos/id/${number}/200/300`}
+            style={{
+                width: '100%',
+                height: 300,
+                marginBottom: 10
+            }}
+        />
+
+        // <Image
+        //     source={{ uri: `https://picsum.photos/id/${number}/200/300` }}
+        //     style={{
+        //         width: '100%',
+        //         height: 300,
+        //         marginBottom: 10
+        //     }}
+        // />
+
+    )
+}
